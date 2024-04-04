9b937593a2cb2eaeec9ea9000643f27668b3a567
Dana Popa <popa.dana100@gmail.com>
2024-04-04 15:09:17 +0300

    task 7

---
+++ task7/src/App.jsx
@@ -0,0 +1,13 @@
+import Counter from "./Counter";
+import "./styles.css";
+
+const App = () => {
+  return (
+    <main>
+      <h1>React State and useState Exercise</h1>
+      <Counter />
+    </main>
+  );
+};
+
+export default App;
